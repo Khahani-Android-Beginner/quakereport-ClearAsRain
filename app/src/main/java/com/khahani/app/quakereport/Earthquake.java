51f1b7176ba42e2424a6958e70f11560929442f0
Mohammadreza Khahani <mr.khahani@gmail.com>
2018-07-22 07:28:07 -0700

    Create Earthquake Class

---
+++ app/src/main/java/com/khahani/app/quakereport/Earthquake.java
@@ -0,0 +1,27 @@
+package com.khahani.app.quakereport;
+
+
+
+public class Earthquake {
+    private String mCity;
+    private float mMagnit;
+    private long mDate;
+
+    public Earthquake(String mCity, float mMagnit, long mDate) {
+        this.mCity = mCity;
+        this.mMagnit = mMagnit;
+        this.mDate = mDate;
+    }
+
+    public String getCity() {
+        return mCity;
+    }
+
+    public float getMagnit() {
+        return mMagnit;
+    }
+
+    public long getDate() {
+        return mDate;
+    }
+}
